531bdefb847001ce98679b0a8e15de47d3f87152
Jonathan Giroux <jonathan.giroux@studio100animation.com>
2019-05-15 12:47:30 +0200

    Update

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,54 @@
 import { bootstrap } from "aurelia-bootstrapper";
+import { Aurelia, LoaderPlugin } from "aurelia-framework";
+import { PLATFORM } from "aurelia-pal";
+import { Loader } from "aurelia-loader";
+import "aurelia-framework";
+import "aurelia-pal-browser";
 
-bootstrap(function(aurelia) {
+class CustomLoader extends Loader {
+  private modules: any = {};
+
+  map(id: string, source: string): void {}
+
+  normalizeSync(moduleId: string, relativeTo: string): string {
+    return moduleId;
+  }
+
+  normalize(moduleId: string, relativeTo: string): Promise<string> {
+    return Promise.resolve(moduleId);
+  }
+
+  loadModule(id: string): Promise<any> {
+    return import(id);
+  }
+
+  loadAllModules(ids: string[]): Promise<any[]> {
+    return Promise.all(ids.map(id => this.loadModule(id)));
+  }
+
+  loadTemplate(url: string): Promise<TemplateRegistryEntry> {
+    throw new Error("Loader must implement loadTemplate(url).");
+  }
+
+  loadText(url: string): Promise<string> {
+    throw new Error("Loader must implement loadText(url).");
+  }
+
+  applyPluginToUrl(url: string, pluginName: string): string {
+    return `${pluginName}!${url}`;
+  }
+
+  addPlugin(pluginName: string, implementation: LoaderPlugin): void {
+    throw new Error(
+      "Loader must implement addPlugin(pluginName, implementation)."
+    );
+  }
+}
+
+PLATFORM.Loader = CustomLoader;
+
+bootstrap(function(aurelia: Aurelia) {
   aurelia.use.standardConfiguration().developmentLogging();
 
-  aurelia.start().then(() => aurelia.setRoot("./dist/app", document.body));
+  aurelia.start().then(() => aurelia.setRoot("./app", document.body));
 });
